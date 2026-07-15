cd6c552b32dbddf0d5ce742fb2338f761080191b
arissachen2014-lang <arissachen2014@gmail.com>
2026-07-15 15:24:17 +0800

    123

--- pages/class3-1.py
+++ pages/class3-1.py
@@ -14,3 +14,5 @@ col1, col2, col3 = st.columns([1, 2, 3])
 col1.button("按鈕1", key="btn5")  # 在col1中建立一個按鈕類似st.button("按鈕1")
 col2.button("按鈕2", key="btn6")  # 在col2中建立一個按鈕類似st.button("按鈕2")
 col3.button("按鈕3", key="btn7")  # 在col3中建立一個按鈕類似st.button("按鈕3")
+
+# 
